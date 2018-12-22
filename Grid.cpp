1ee63cea65b6ccd45015119c3b6738d69456ffd6
berkay <berkay270@gmail.com>
2018-12-22 17:30:21 +0300

    strange things happen

--- Grid.cpp
+++ Grid.cpp
@@ -1,35 +1,39 @@
 #include<iostream>
+#include <cmath>
 #include "Grid.h"
 
+template <class T>
+T** array2d(int m, int n){
+  T** A = new T*[m];
+  for(int i=0; i<m; i++){
+    A[i] = new T[n];
+  }
+  return A;
+}
+
 Grid::Grid(double m_x, double m_y, double m_dx, double m_dy, bool m_flat) : x_length(m_x), y_length(m_y), dx(m_dx), dy(m_dy), isflat(m_flat){} 
-//void Grid::set(double m_x, double m_y, double m_dx, double m_dy ){
-//  x = m_x;
-//  y = m_x;
-//  dx = m_dx;
-//  dy = m_dy;
-//  std::cout<<"x : " << x << std::endl;
-//  std::cout<<"y : " << y << std::endl;
-//  std::cout<<"dx : " << dx << std::endl;
-//  std::cout<<"dy : " << dy << std::endl;
-//}
 void Grid::setDepth(double m_h){
   grid_dimx= x_length/dx+2;
   grid_dimy = y_length/dy+2;
-  h = new double*[grid_dimx];
+  h0 = new double*[grid_dimx];
   //initialize h
   if(isflat){
     for(int i = 0; i < grid_dimx; i++){
-      h[i] = new double[grid_dimy];
+      h0[i] = new double[grid_dimy];
     }
     for(int i = 1; i < grid_dimx-1; i++){
       for(int j = 1; j < grid_dimy-1; j++){
-        h[i][j] = m_h;
+        h0[i][j] = m_h;
 
 //        std::cout << i << " : " << j << " = " << h[i][j]<< std::endl;
       }
-    for(int i = 1; i < grid_dimx-1; i++){
-        h[i][0] = 0;
-        h[0][i] = 0;
+    for(int i = 0; i < grid_dimx; i++){
+        h0[i][0] = 0;
+        h0[i][grid_dimy-1] = 0;
+    }
+    for(int i = 0; i < grid_dimy; i++){
+        h0[0][i] = 0;
+        h0[grid_dimx-1][i] = 0;
     }
     }
     
@@ -66,5 +70,99 @@ void Grid::show(){
 void Grid::maxDepth(){
 }
 void Grid::minDepth(){
+}
+void Grid::initialize(int mode=1){
+  h = array2d<double>(grid_dimx,grid_dimy);
+  u = array2d<double>(grid_dimx,grid_dimy);
+  v = array2d<double>(grid_dimx,grid_dimy);
+  zeta = array2d<double>(grid_dimx,grid_dimy);
+  zeta_star = array2d<double>(grid_dimx,grid_dimy);
+  if(mode==1){
+    for(int k=0; k<grid_dimx;k++){
+      for(int j=0; j<grid_dimy;j++){
+        zeta[k][j] = 0.0;
+        zeta_star[k][j] = 0.0;
+        h[k][j] = h0[k][j]+zeta[k][j];
+        u[k][j] = 0.0;
+        v[k][j] = 0.0;
+      }
+    }
+    zeta[grid_dimx/2][grid_dimy/2] = 1.0;
+  }
+  initialized = 1;
+}
+
+void Grid::solve(Grid myGrid, float dt,float time, float epsilon){
+    if(!initialized){
+        std::cout << "parameters is not initialized"  << std::endl;
+    }else{
+    // Test stability criteria
+    double min_spacing = myGrid.get_dx();
+    if (min_spacing > myGrid.get_dy())
+        min_spacing = myGrid.get_dy();
+    if(dt > (min_spacing / (sqrt(2*gravity*10)) )){
+        std::cout << "Error! Stability Criteria is not satisfied." << std::endl;
+    }
+    double x = myGrid.get_x();
+    double y = myGrid.get_y();
+    double he,hw,hn,hs;
+    NcFile ofile("output.nc", NcFile::Replace);
+    // add dimensions
+    NcDim *dim_x = ofile.add_dim("x", grid_dimx);
+    NcDim *dim_y = ofile.add_dim("y", grid_dimy);
+    NcDim *dim_t = ofile.add_dim("time");
+
+   // create variables
+    NcVar *var_z = ofile.add_var("zeta", ncFloat, dim_t, dim_y, dim_x);
+    var_z->add_att("units", "meter");
+    var_z->add_att("long_name", "free surface height");
+
+    std::cout << h[grid_dimx/2][grid_dimy/2] << std::endl;
+    // simulation loop
+    for(int t=0;t<=time/dt;t++){
+       for(int j=1;j<grid_dimy-1;j++){
+         for(int k=1;k<grid_dimx-1;k++){
+           u[j][k]=u[j][k]-dt*gravity*(zeta[j][k+1]-zeta[j][k])/dx;
+           v[j][k]=v[j][k]-dt*gravity*(zeta[j+1][k]-zeta[j][k])/dy;
+         }
+        }
+       for(int j=1;j<grid_dimy-1;j++){
+         for(int k=1;k<grid_dimx-1;k++){
+           if(u[j][k]>0)
+                   he=h[j][k];
+           else if(u[j][k]<0)
+                   he=h[j][k+1];
+           if(u[j][k-1]>0)
+                   hw=h[j][k-1];
+           else if(u[j][k-1]<0)
+                   hw=h[j][k];
+           if(v[j][k]>0)
+                   hn=h[j][k];
+           else if(v[j][k]<0)
+                   hn=h[j+1][k];
+           if(v[j-1][k]>0)
+                   hs=h[j-1][k];
+           else if(v[j-1][k]<0)
+                   hs=h[j][k];
+           zeta_star[j][k]=zeta[j][k]-dt*(u[j][k]*he-u[j][k-1]*hw)/dx-dt*(v[j][k]*hn-v[j-1][k]*hs)/dy;
+           // shapiro filter
+           zeta[j][k]=(1-epsilon)*zeta_star[j][k]+0.25*epsilon*(zeta_star[j][k-1]+zeta_star[j][k+1]+zeta_star[j-1][k]+zeta_star[j+1][k]);
+         }
+       }
+       for(int j=1;j<grid_dimy-1;j++){
+         for(int k=1;k<grid_dimx-1;k++){
+           h[j][k] = h0[j][k] + zeta[j][k];
+         }
+       }
+       if(t%5==0){
+         std::cout <<  zeta[grid_dimx/2][grid_dimy/2] << std::endl;
+         std::cout <<  t/5 << std::endl;
+         var_z->put_rec(&zeta[0][0],t/5);
+       }
+//
+    }
+    ofile.close();
+//
+  }
 }
 
--- Grid.h
+++ Grid.h
@@ -1,15 +1,25 @@
 #ifndef GRID_H
 #define GRID_H
+#include <netcdfcpp.h>
+const double gravity = 9.81;
+
 class Grid{
   private:
     double x_length; // length in x direction
     double y_length; // length in y direction
+    double** h0; // the depth in 2 dimension
     double** h; // the depth in 2 dimension
+    double** u; // the depth in 2 dimension
+    double** v; // the depth in 2 dimension
+    double** zeta; // the depth in 2 dimension
+    double** zeta_star; // the depth in 2 dimension
     //grid parameters
     double dx; //grid spacing in x direction
     double dy; // grid spacing in y direction
     bool isflat; // the depth type
+    bool initialized; // the depth type
     int grid_dimx, grid_dimy;
+    
   public:
     Grid(double, double, double, double, bool);
     void setDepth(double m_h);
@@ -23,6 +33,8 @@ class Grid{
     void show();
     void maxDepth();
     void minDepth();
+    void initialize(int mode);
+    void solve(Grid myGrid, float dt,float time, float epsilon);
 
 };
 #endif
